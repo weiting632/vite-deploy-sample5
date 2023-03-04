9ba24cd00dc63844892036a0d603f3f09ba2b410
weiting632 <alan885544@gmail.com@example.com>
2023-03-05 03:56:01 +0800

    deploy

---
+++ assets/AboutView-6573093b.js
@@ -0,0 +1,1 @@
+import{_ as s,r as n,o as r,c,b as o,w as a,d as e,F as _,a as u}from"./index-c4bad56e.js";const l={},d=u("h1",null,"This is About page.",-1);function i(p,f,m,b,h,x){const t=n("router-link");return r(),c(_,null,[d,o(t,{to:"/"},{default:a(()=>[e("Home")]),_:1}),e(" | "),o(t,{to:"/about"},{default:a(()=>[e("About")]),_:1})],64)}const V=s(l,[["render",i]]);export{V as default};
